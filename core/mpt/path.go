4435387e567627388db6d7e086d0f43fb99e3675
Mikhail Sherstennikov <mike.sherstennikov@gmail.com>
2024-05-05 18:58:17 +0300

    Add Merkle Patricia Trie
    Implementation is based on https://github.com/popzxc/merkle-patricia-trie
    RLP serialization is replaced by SSZ

---
+++ core/mpt/path.go
@@ -0,0 +1,193 @@
+package mpt
+
+import (
+	common "github.com/NilFoundation/nil/common"
+	"github.com/NilFoundation/nil/core/ssz"
+)
+
+const (
+	OddFlag  = 0x10
+	LeafFlag = 0x20
+)
+
+type Path struct {
+	data   []byte
+	offset int
+	IsLeaf bool
+}
+
+var _ ssz.SizedObjectSSZ = (*Path)(nil)
+
+type PathAccessor interface {
+	At(idx int) int
+}
+
+func newPath(data []byte, offset int) *Path {
+	return &Path{data, offset, false}
+}
+
+func (path *Path) Size() int {
+	return len(path.data)*2 - path.offset
+}
+
+func (path *Path) Empty() bool {
+	return path.Size() == 0
+}
+
+func (path *Path) Equal(other *Path) bool {
+	if other.Size() != path.Size() {
+		return false
+	}
+	for i := 0; i < path.Size(); i++ {
+		if path.At(i) != other.At(i) {
+			return false
+		}
+	}
+	return true
+}
+
+func (path *Path) StartsWith(other *Path) bool {
+	if other.Size() > path.Size() {
+		return false
+	}
+
+	for i := 0; i < other.Size(); i++ {
+		if path.At(i) != other.At(i) {
+			return false
+		}
+	}
+	return true
+}
+
+func (path *Path) At(idx int) int {
+	idx += path.offset
+	nibbleIdx := idx % 2
+
+	var b = int(path.data[idx/2])
+
+	if nibbleIdx == 0 {
+		return b >> 4
+	} else {
+		return b & 0x0F
+	}
+}
+
+func (path *Path) Consume(amount int) *Path {
+	path.offset += amount
+	return path
+}
+
+func createNew[T PathAccessor](path T, length int) *Path {
+	data := make([]byte, 0, length)
+
+	isOddLen := length%2 == 1
+	pos := 0
+
+	if isOddLen {
+		data = append(data, byte(path.At(pos)))
+		pos += 1
+	}
+
+	for pos < length {
+		data = append(data, byte(path.At(pos)*16+path.At(pos+1)))
+		pos += 2
+	}
+
+	var offset int
+	if isOddLen {
+		offset = 1
+	} else {
+		offset = 0
+	}
+
+	return newPath(data, offset)
+}
+
+func (path *Path) CommonPrefix(other *Path) *Path {
+	leastLen := min(path.Size(), other.Size())
+	commonLen := 0
+	for i := 0; i < leastLen; i++ {
+		if path.At(i) != other.At(i) {
+			break
+		}
+		commonLen += 1
+	}
+	return createNew(path, commonLen)
+}
+
+func (path *Path) Encode() []byte {
+	output := make([]byte, 0, path.Size())
+
+	nibblesLen := path.Size()
+	isOdd := nibblesLen%2 == 1
+
+	prefix := byte(0x00)
+	if isOdd {
+		prefix += byte(OddFlag + path.At(0))
+	}
+	if path.IsLeaf {
+		prefix += LeafFlag
+	}
+
+	output = append(output, prefix)
+
+	pos := nibblesLen % 2
+
+	for pos < nibblesLen {
+		b := path.At(pos)*16 + path.At(pos+1)
+		output = append(output, byte(b))
+		pos += 2
+	}
+
+	return output
+}
+
+type Chained struct {
+	first  *Path
+	second *Path
+}
+
+func (c *Chained) At(idx int) int {
+	if idx < c.first.Size() {
+		return c.first.At(idx)
+	} else {
+		return c.second.At(idx - c.first.Size())
+	}
+}
+
+func (c *Chained) Size() int {
+	return c.first.Size() + c.second.Size()
+}
+
+func (path *Path) Combine(other *Path) *Path {
+	c := Chained{path, other}
+	return createNew(&c, c.Size())
+}
+
+func (path *Path) EncodeSSZ(buf []byte) ([]byte, error) {
+	return append(buf, path.Encode()...), nil
+}
+
+func (path *Path) EncodingSizeSSZ() int {
+	return 0
+}
+
+func (path *Path) DecodeSSZ(buf []byte, version int) error {
+	isOddLen := (buf[0] & OddFlag) == OddFlag
+	if isOddLen {
+		path.offset = 1
+	} else {
+		path.offset = 2
+	}
+	path.data = make([]byte, len(buf))
+	copy(path.data, buf)
+	return nil
+}
+
+func (path *Path) Static() bool {
+	return false
+}
+
+func (path *Path) Clone() common.Clonable {
+	return path
+}
---
+++ core/mpt/path_test.go
@@ -0,0 +1,72 @@
+package mpt
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAt(t *testing.T) {
+	data := [2]byte{0x12, 0x34}
+	nibbles := newPath(data[:], 0)
+	require.Equal(t, 1, nibbles.At(0))
+	require.Equal(t, 2, nibbles.At(1))
+	require.Equal(t, 3, nibbles.At(2))
+	require.Equal(t, 4, nibbles.At(3))
+}
+
+func TestAtWithOffset(t *testing.T) {
+	data := [2]byte{0x12, 0x34}
+	nibbles := newPath(data[:], 1)
+	require.Equal(t, 2, nibbles.At(0))
+	require.Equal(t, 3, nibbles.At(1))
+	require.Equal(t, 4, nibbles.At(2))
+	assert.Panics(t, func() { nibbles.At(3) }, "Should panic")
+}
+
+func TestEncode(t *testing.T) {
+	nibbles := newPath(([]byte{0x12, 0x34})[:], 0)
+	require.Equal(t, nibbles.Encode(), ([]byte{0x00, 0x12, 0x34})[:])
+	nibbles.IsLeaf = true
+	require.Equal(t, nibbles.Encode(), ([]byte{0x20, 0x12, 0x34})[:])
+
+	nibbles = newPath(([]byte{0x12, 0x34})[:], 1)
+	require.Equal(t, nibbles.Encode(), ([]byte{0x12, 0x34})[:])
+	nibbles.IsLeaf = true
+	require.Equal(t, nibbles.Encode(), ([]byte{0x32, 0x34})[:])
+}
+
+func TestCommonPrefix(t *testing.T) {
+	nibblesA := newPath(([]byte{0x12, 0x34})[:], 0)
+	nibblesB := newPath(([]byte{0x12, 0x56})[:], 0)
+	common := nibblesA.CommonPrefix(nibblesB)
+	require.True(t, common.Equal(newPath(([]byte{0x12})[:], 0)))
+
+	nibblesA = newPath(([]byte{0x12, 0x34})[:], 0)
+	nibblesB = newPath(([]byte{0x12, 0x36})[:], 0)
+	common = nibblesA.CommonPrefix(nibblesB)
+	require.True(t, common.Equal(newPath(([]byte{0x01, 0x23})[:], 1)))
+
+	nibblesA = newPath(([]byte{0x12, 0x34})[:], 1)
+	nibblesB = newPath(([]byte{0x12, 0x56})[:], 1)
+	common = nibblesA.CommonPrefix(nibblesB)
+	require.True(t, common.Equal(newPath(([]byte{0x12})[:], 1)))
+
+	nibblesA = newPath(([]byte{0x52, 0x34})[:], 0)
+	nibblesB = newPath(([]byte{0x02, 0x56})[:], 0)
+	common = nibblesA.CommonPrefix(nibblesB)
+	require.True(t, common.Equal(newPath(([]byte{})[:], 0)))
+}
+
+func TestCombine(t *testing.T) {
+	nibblesA := newPath(([]byte{0x12, 0x34})[:], 0)
+	nibblesB := newPath(([]byte{0x56, 0x78})[:], 0)
+	common := nibblesA.Combine(nibblesB)
+	require.True(t, common.Equal(newPath(([]byte{0x12, 0x34, 0x56, 0x78})[:], 0)))
+
+	nibblesA = newPath(([]byte{0x12, 0x34})[:], 1)
+	nibblesB = newPath(([]byte{0x56, 0x78})[:], 3)
+	common = nibblesA.Combine(nibblesB)
+	toCompare := newPath(([]byte{0x23, 0x48})[:], 0)
+	require.True(t, common.Equal(toCompare))
+}
