45dd2fb7ee8d5d6e1fee95f10545d86b11bb5914
Alexander Myltsev <a.myltsev@gmail.com>
2024-05-10 17:16:03 +0400

    Move unit tests to their respective packages.

--- core/ssz/ssz_test.go
+++ core/ssz/ssz_test.go
@@ -1,11 +1,10 @@
-package tests
+package ssz
 
 import (
 	"encoding/hex"
 	"testing"
 
 	common "github.com/NilFoundation/nil/common"
-	"github.com/NilFoundation/nil/core/ssz"
 	types "github.com/NilFoundation/nil/core/types"
 	"github.com/holiman/uint256"
 	"github.com/stretchr/testify/require"
@@ -29,7 +28,7 @@ func TestSsz(t *testing.T) {
 	require.Equal(t, block2.PrevBlock, block.PrevBlock)
 	require.Equal(t, block2.SmartContractsRoot, block.SmartContractsRoot)
 
-	h, err := ssz.SSZHash(&block2)
+	h, err := SSZHash(&block2)
 	require.NoError(t, err)
 
 	h2, err := hex.DecodeString("105d380db7f5773ffd3d99f86ef08ddd354be37962457bfdc968e739b0bea4e4")
@@ -62,7 +61,7 @@ func TestSszTransaction(t *testing.T) {
 	require.Equal(t, message2.Data, message.Data)
 	require.Equal(t, message2.Signature, message.Signature)
 
-	h, err := ssz.SSZHash(&message2)
+	h, err := SSZHash(&message2)
 	require.NoError(t, err)
 
 	h2, err := hex.DecodeString("25d61cdf3ba63fcc5f96505be3c2e5b3f5dcdfc6527216c9172f8b5def08bff1")
