472fc6ea318f6e52f43d246c1c8db1fca239128a
Alexander Myltsev <a.myltsev@gmail.com>
2024-05-13 17:15:32 +0400

    Auto-create account on SetBalance.

--- core/shardchain/shardchain.go
+++ core/shardchain/shardchain.go
@@ -78,11 +78,7 @@ func (c *ShardChain) testTransaction(ctx context.Context, nshards int) (common.H
 
 	addr := common.BytesToAddress([]byte("contract-" + strconv.Itoa(c.Id)))
 
-	accountState, err := es.GetAccount(addr)
-
-	if err != nil {
-		return common.EmptyHash, err
-	}
+	accountState := es.GetAccount(addr)
 
 	value := uint256.Int{}
 	gas := uint64(1000000)
