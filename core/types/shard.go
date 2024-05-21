8b2a5b24471ea2ecb145e817eb4600384ceff6fd
Иванов Владимир Валерьевич <vl.ivanov@2gis.ru>
2024-05-21 16:01:58 +0700

    Reduce the size of ShardId from 64 to 32 bits.

--- core/types/shard.go
+++ core/types/shard.go
@@ -5,16 +5,17 @@ import (
 	"strconv"
 )
 
-type ShardId uint64
+// 32 bits are more than enough while avoiding problems with marshaling 64-bit values as numbers in JSON.
+type ShardId uint32
 
 const MasterShardId = ShardId(0)
 
 func (s ShardId) MarshalJSON() ([]byte, error) {
-	return json.Marshal(uint64(s))
+	return json.Marshal(uint32(s))
 }
 
 func (s *ShardId) UnmarshalJSON(data []byte) error {
-	var id uint64
+	var id uint32
 	if err := json.Unmarshal(data, &id); err != nil {
 		return err
 	}
