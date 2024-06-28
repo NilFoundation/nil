67532b5a8cbbf9e819de32b9f68cacd59d2d5963
Konstantin Nazarov <mail@knazarov.com>
2024-06-28 21:37:40 +0100

    Fix "nil_cli wallet send-message" default amount being non-zero
    In the CLI parameter parsing, there is an error when `nil_cli wallet new`
    and `nil_cli wallet send-message` reuse the same parameter variable
    for specifying the amount of tokens to send.
    
    In `nil_cli wallet send-message, the default value is 0, which it
    should be. But it gets overriden by the other command.
    
    What this does is that if you don't specify `--amount` and call a
    non-payable method, you'll essentially still pass about 10m tokens in
    the message, and there would be "execution reverted" error down the
    line.
    
    This patch is simple -- let's use different variables for the two
    different commands.

--- cmd/nil_cli/wallet/params.go
+++ cmd/nil_cli/wallet/params.go
@@ -17,12 +17,13 @@ const (
 var params = &walletParams{}
 
 type walletParams struct {
-	abiPath    string
-	amount     types.Uint256
-	noWait     bool
-	salt       types.Uint256
-	shardId    types.ShardId
-	gasLimit   types.Uint256
-	currency   types.Uint256
-	currencies []string
+	abiPath           string
+	amount            types.Uint256
+	new_wallet_amount types.Uint256
+	noWait            bool
+	salt              types.Uint256
+	shardId           types.ShardId
+	gasLimit          types.Uint256
+	currency          types.Uint256
+	currencies        []string
 }
