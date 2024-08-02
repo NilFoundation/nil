b368f2edd15f33eadf5fe84d96a0297ca2103144
Dmitrii Skopintsev <dmtrskv@nil.foundation>
2024-08-02 22:01:00 +0400

    Rename nil -> nild, nil_cli -> nil

---
+++ nil/cmd/nil/internal/contract/balance.go
@@ -0,0 +1,43 @@
+package contract
+
+import (
+	"fmt"
+
+	"github.com/NilFoundation/nil/nil/cmd/nil/internal/common"
+	"github.com/NilFoundation/nil/nil/cmd/nil/internal/config"
+	"github.com/NilFoundation/nil/nil/internal/types"
+	"github.com/NilFoundation/nil/nil/services/cliservice"
+	"github.com/spf13/cobra"
+)
+
+func GetBalanceCommand(cfg *common.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "balance [address]",
+		Short: "Returns a smart contract balance",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runBalance(cmd, args, cfg)
+		},
+		SilenceUsage: true,
+	}
+
+	return cmd
+}
+
+func runBalance(_ *cobra.Command, args []string, cfg *common.Config) error {
+	var address types.Address
+	if err := address.Set(args[0]); err != nil {
+		return err
+	}
+
+	service := cliservice.NewService(common.GetRpcClient(), cfg.PrivateKey)
+	balance, err := service.GetBalance(address)
+	if err != nil {
+		return err
+	}
+	if !config.Quiet {
+		fmt.Print("Contract balance: ")
+	}
+	fmt.Println(balance)
+	return nil
+}
---
+++ nil/cmd/nil/internal/minter/create-currency.go
@@ -0,0 +1,53 @@
+package minter
+
+import (
+	"fmt"
+
+	"github.com/NilFoundation/nil/nil/cmd/nil/internal/common"
+	"github.com/NilFoundation/nil/nil/cmd/nil/internal/config"
+	"github.com/NilFoundation/nil/nil/internal/types"
+	"github.com/NilFoundation/nil/nil/services/cliservice"
+	"github.com/spf13/cobra"
+)
+
+func CreateCurrencyCommand(cfg *common.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-currency [address] [amount] [name]",
+		Short: "Create wallet/contract currency",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runCreateCurrency(cmd, args, cfg)
+		},
+		SilenceUsage: true,
+	}
+
+	cmd.Flags().BoolVar(&params.withdraw, withdrawFlag, false, "Withdraw created currency to the wallet/contract")
+
+	return cmd
+}
+
+func runCreateCurrency(_ *cobra.Command, args []string, cfg *common.Config) error {
+	service := cliservice.NewService(common.GetRpcClient(), cfg.PrivateKey)
+
+	var address types.Address
+	if err := address.Set(args[0]); err != nil {
+		return err
+	}
+
+	var amount types.Value
+	if err := amount.Set(args[1]); err != nil {
+		return err
+	}
+
+	name := args[2]
+
+	currencyId, err := service.CurrencyCreate(address, amount, name, params.withdraw)
+	if err != nil {
+		return err
+	}
+	if !config.Quiet {
+		fmt.Print("Created Currency ID: ")
+	}
+	fmt.Println(currencyId)
+	return nil
+}
