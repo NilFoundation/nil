4851c3b848af4f77ff3e6c170d7a8a6f1846942b
Zerg1996 <alalmoskvin@gmail.com>
2024-08-07 16:19:10 +0200

    Calculate hash from reflection

--- nil/internal/types/version_info.go
+++ nil/internal/types/version_info.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"fmt"
+	"reflect"
+
 	fastssz "github.com/NilFoundation/fastssz"
 	"github.com/NilFoundation/nil/nil/common"
 )
@@ -15,14 +18,22 @@ var (
 	_ fastssz.Unmarshaler = new(VersionInfo)
 )
 
-var SchemesInsideDb = []common.Hash{new(SmartContract).Hash(), new(Block).Hash(), new(Message).Hash()}
+var SchemesInsideDb = []interface{}{SmartContract{}, Block{}, Message{}, ExternalMessage{}, InternalMessagePayload{}}
 
 const SchemeVersionInfoKey = "SchemeVersionInfo"
 
 func NewVersionInfo() *VersionInfo {
 	var res []byte
-	for _, hash := range SchemesInsideDb {
-		res = append(res, hash.Bytes()...)
+	for _, curStruct := range SchemesInsideDb {
+		v := reflect.ValueOf(curStruct)
+		for i := range v.NumField() {
+			name := v.Type().Field(i).Name
+			res = append(res, common.PoseidonHash([]byte(name)).Bytes()...)
+
+			value := v.Field(i).Interface()
+			valueStr := fmt.Sprintf("%v", value)
+			res = append(res, common.PoseidonHash([]byte(valueStr)).Bytes()...)
+		}
 	}
 	return &VersionInfo{Version: common.PoseidonHash(res)}
 }
