acc6ad153c90558adf75a60d1c4ade9d2ddbf1eb
khannanov-nil <khannanov@nil.foundation>
2024-09-13 09:15:44 +0000

    attempt to fix deps
    interrim commit
    
    added Playground integration
    
    fix for CI
    
    bug fixes
    
    misc fixes
    
    added hash genetator
    
    update to script
    
    fixed hash generation in Nix
    
    final fix to playground
    
    formatter changes

---
+++ docs/plugins/nil-playground-plugin/index.js
@@ -0,0 +1,45 @@
+import fs from 'node:fs/promises';
+import crypto from 'node:crypto';
+import path from 'path';
+import { ExecutionRevertedError } from 'viem';
+
+
+export default async function nilPlaygroundPlugin(context, options) {
+  const { contracts } = options;
+  return {
+    name: 'nil-playground-plugin',
+    getThemePath() {
+      return path.resolve(__dirname, 'theme');
+    },
+    async loadContent() {
+
+
+      const getCodes = async () => {
+        const contractCodes = {};
+        const tests = path.resolve(__dirname, '../../tests');
+        const files = await fs.readdir(tests, {});
+
+        for (const file of files) {
+          const filePath = path.resolve(tests, file);
+          console.log(filePath);
+          if (filePath.endsWith('.sol')) {
+            const data = await fs.readFile(filePath, 'utf8');
+            contractCodes[file] = data;
+
+          }
+        }
+        return contractCodes;
+      };
+
+
+      const result = await getCodes();
+      return result;
+
+    },
+    async contentLoaded({ content, actions }) {
+      const { setGlobalData } = actions;
+      setGlobalData({ contractCodes: content });
+    }
+  };
+}
+
