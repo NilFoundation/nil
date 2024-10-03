d250d1dfae63c9cfddb6d498480331f6a3b4317d
zadykian <zadykian@outlook.com>
2024-10-03 12:51:21 +0400

    Sync Committee: * Makefile.inc: Added task_handler_generated_mock target; declared deps for mock targets * generate.go: Adding `test` build tag to generated mock files ;

--- nil/services/synccommittee/internal/api/generate.go
+++ nil/services/synccommittee/internal/api/generate.go
@@ -1,4 +1,7 @@
 package api
 
 //go:generate go run github.com/matryer/moq -out task_request_handler_generated_mock.go -rm -stub -with-resets . TaskRequestHandler
+//go:generate bash -c "echo -e \"//go:build test\n$(cat task_request_handler_generated_mock.go)\" > task_request_handler_generated_mock.go"
+
 //go:generate go run github.com/matryer/moq -out task_handler_generated_mock.go -rm -stub -with-resets . TaskHandler
+//go:generate bash -c "echo -e \"//go:build test\n$(cat task_handler_generated_mock.go)\" > task_handler_generated_mock.go"
