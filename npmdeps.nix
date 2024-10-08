a7ce55f138d05527ea1fe9f0c86c915880575fb7
Konstantin Nazarov <mail@knazarov.com>
2024-10-08 21:50:32 +0100

    Update package-lock.json

--- npmdeps.nix
+++ npmdeps.nix
@@ -15,5 +15,5 @@ in
       ./smart-contracts/package.json
     ];
   };
-  hash = "sha256-td4N88meNuxmm61cTclR9kRrC8Vs59V4E2Aslnrifvs=";
+  hash = "sha256-gNeabiWOhrz9jLvedDAshZ954V55veDFHIaIYVfYN64=";
 })
