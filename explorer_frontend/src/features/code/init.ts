c59d55d704db9e627bd68c1f8655318494838587
Gezort <mirlev95@gmail.com>
2024-10-20 14:13:59 +0100

    upd smart-contracts pkg version and fix contract compilation

--- explorer_frontend/src/features/code/init.ts
+++ explorer_frontend/src/features/code/init.ts
@@ -28,8 +28,9 @@ persist({
 
 compileCodeFx.use(async ({ version, code }) => {
   const compiler = await fetchSolidityCompiler(`https://binaries.soliditylang.org/bin/${version}`);
+  // TODO: handle this gracefully(?)
   const res = await compiler.compile({
-    code,
+    '// SPDX-License-Identifier: MIT\n' + code,
   });
   const contracts: App[] = [];
   if ("contracts" in res && res.contracts !== undefined && "Compiled_Contracts" in res.contracts) {
