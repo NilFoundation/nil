0d309aa9c2da699de1716357f426e75c5618b240
klond90 <newbiecraft@gmail.com>
2024-11-04 11:00:37 +0100

    Cometa in explorer integration

--- explorer_backend/routes/account.ts
+++ explorer_backend/routes/account.ts
@@ -5,6 +5,7 @@ import { ethAddressSchema } from "../validations/AddressScheme";
 import { CurrencySchema } from "../daos/transactions";
 import { hexToBigInt, numberToHex } from "viem";
 import { addHexPrefix } from "@nilfoundation/niljs";
+import { fetchAccountMetadata } from "../daos/account";
 
 export const accountRouter = router({
   state: publicProcedure
@@ -15,12 +16,24 @@ export const accountRouter = router({
         code: z.string(),
         isInitialized: z.boolean(),
         currencies: z.array(CurrencySchema),
+        source_code: z.optional(z.record(z.string())),
+        abi: z.optional(z.any()),
       }),
     )
     .query(async (opts) => {
       const { balance, isInitialized, code, currencies } = await fetchAccountState(
         opts.input as `0x${string}`,
       );
+      let sourceCode: Record<string, string> = {};
+      let abi: unknown = null;
+      try {
+        const metadata = await fetchAccountMetadata(opts.input as `0x${string}`);
+        sourceCode = metadata?.source_code || {};
+        abi = JSON.parse(metadata?.abi || "");
+      } catch (e) {
+        abi = null;
+      }
+
       return {
         balance,
         code,
@@ -32,6 +45,8 @@ export const accountRouter = router({
           });
           return CurrencySchema.parse({ currency: address, balance: balance.toString(10) });
         }),
+        source_code: sourceCode,
+        abi,
       };
     }),
 });
