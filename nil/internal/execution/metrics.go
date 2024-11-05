eaa6dd6a9b897cac58080f559b913baafcdfa1a1
Oleg Babin <babinoleg@mail.ru>
2024-11-05 17:40:46 +0400

    add block generation metrics
    Following metrics were added:
    
      - Amount of external/internal messages
      - CoinsUsed during block generation
      - Current block number and gas price
    
    Part of #1125

---
+++ nil/internal/execution/metrics.go
@@ -0,0 +1,96 @@
+package execution
+
+import (
+	"context"
+
+	"github.com/NilFoundation/nil/nil/common/logging"
+	"github.com/NilFoundation/nil/nil/internal/telemetry"
+	"github.com/NilFoundation/nil/nil/internal/types"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+type MetricsHandler struct {
+	option metric.MeasurementOption
+
+	measurer *telemetry.Measurer
+
+	// Histograms
+	coinsUsedHistogram metric.Int64Histogram
+
+	// Counters
+	internalMsgCounter metric.Int64Counter
+	externalMsgCounter metric.Int64Counter
+
+	// Gauges
+	gasPrice metric.Int64Gauge
+	blockId  metric.Int64Gauge
+}
+
+func NewMetricsHandler(name string, shardId types.ShardId) (*MetricsHandler, error) {
+	meter := telemetry.NewMeter(name)
+	measurer, err := telemetry.NewMeasurer(
+		meter, "block_generation", attribute.Int64(logging.FieldShardId, int64(shardId)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	handler := &MetricsHandler{
+		measurer: measurer,
+		option:   metric.WithAttributes(attribute.Int64(logging.FieldShardId, int64(shardId))),
+	}
+
+	if err := handler.initMetrics(meter); err != nil {
+		return nil, err
+	}
+
+	return handler, nil
+}
+
+func (mh *MetricsHandler) initMetrics(meter metric.Meter) error {
+	var err error
+
+	// Initialize histograms
+	mh.coinsUsedHistogram, err = meter.Int64Histogram("coins_used")
+	if err != nil {
+		return err
+	}
+
+	// Initialize counters
+	mh.internalMsgCounter, err = meter.Int64Counter("total_blocks_processed")
+	if err != nil {
+		return err
+	}
+
+	mh.externalMsgCounter, err = meter.Int64Counter("total_errors_encountered")
+	if err != nil {
+		return err
+	}
+
+	// Initialize gauges
+	mh.gasPrice, err = meter.Int64Gauge("gas_price")
+	if err != nil {
+		return err
+	}
+
+	mh.blockId, err = meter.Int64Gauge("block_id")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mh *MetricsHandler) StartProcessingMeasurement(ctx context.Context, gasPrice types.Value, blockId types.BlockNumber) {
+	mh.measurer.Restart()
+	mh.gasPrice.Record(ctx, int64(gasPrice.Uint64()), mh.option)
+	mh.gasPrice.Record(ctx, int64(blockId), mh.option)
+}
+
+func (mh *MetricsHandler) EndProcessingMeasurement(ctx context.Context, internalCnt, externalCnt int64, coinsUsed types.Value) {
+	mh.measurer.Measure(ctx)
+	mh.internalMsgCounter.Add(ctx, internalCnt, mh.option)
+	mh.externalMsgCounter.Add(ctx, externalCnt, mh.option)
+	mh.coinsUsedHistogram.Record(ctx, int64(coinsUsed.Uint64()), mh.option)
+}
