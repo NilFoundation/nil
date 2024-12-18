79e8f7508420c093b3a999f7a88bcdd39dbc9e42
rolaman <rolamanbuss@yandex.ru>
2024-12-18 16:35:46 +0400

    Explorer: Outgoing messages counter

--- explorer_frontend/src/features/transaction/types/Transaction.ts
+++ explorer_frontend/src/features/transaction/types/Transaction.ts
@@ -1,5 +1,6 @@
-import type { fetchTransactionByHash } from "../../../api/transaction";
+import type { fetchTransactionByHash, getChildTransactionsByHash } from "../../../api/transaction";
 
 export type Transaction = Awaited<ReturnType<typeof fetchTransactionByHash>>;
+export type TransactionChilds = Awaited<ReturnType<typeof getChildTransactionsByHash>>;
 
 export type Currency = Transaction["currency"];
