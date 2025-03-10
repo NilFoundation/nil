2b01338851b357ae1e356d6a12c94007ab7db642
x-mass <36629999+x-mass@users.noreply.github.com>
2025-03-10 13:29:34 +0000

    refactor(synccommittee): Use `execution.StateDB` instead of custom one in tracer
    Refactored the tracer to use `execution.StateDB` instead of a custom `vm.StateDB` implementation. This change enables comparison between the original block hash and the traced block hash, ensuring all operations remain consistent.
    
    Key changes:
    - Introduced `OnTxStart` and `OnTxEnd` hooks in the tracing package, moving all per-transaction tracer calls into these hooks.
    - Modified the `GetContract` method in the debug API to include proof of absence when a contract does not exist. This proof is either the longest matched path with the queried contract path or the root node if no path matches at all. This ensures we can prove the absence of even the beginning of such a path in the trie. Includes adjustment to `mpt.BuildProof` function.
    - Refactored the ZKEVM tracer to avoid relying on values unavailable in the EVM tracing hook context. To fetch the current code when needed, added a `Code()` method to `tracing.OpContext`.
    - Significantly changed the MPT tracer: it now computes differences between tries by traversing a trie with the initial root and comparing it against a trie with the current root. This eliminates the need to track changed values during tracing—only the initial and current roots are required, along with the underlying database storing all accessed values.

--- nil/services/synccommittee/prover/tracer/errors.go
+++ nil/services/synccommittee/prover/tracer/errors.go
@@ -6,8 +6,10 @@ import (
 )
 
 var (
-	ErrCantProofGenesisBlock = errors.New("can't prove genesis block")
-	ErrTraceNotFinalized     = errors.New("trace logic malformed: previous opcode not finalized")
+	ErrCantProofGenesisBlock   = errors.New("can't prove genesis block")
+	ErrTraceNotFinalized       = errors.New("trace logic malformed: previous opcode not finalized")
+	ErrTracedBlockHashMismatch = errors.New("generated traced block and fetched block hashes are not equal")
+	ErrClientReturnedNilBlock  = errors.New("client returned nil block")
 )
 
 type managedTracerFailureError struct {
