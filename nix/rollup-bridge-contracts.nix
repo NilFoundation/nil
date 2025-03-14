103b5346548d0fd7ae9f092ab4fb529d9372e427
Andrei Malashkin <andrei.malashkin@nil.foundation>
2025-03-14 13:53:38 +0100

    add forge dependencies from repos

--- nix/rollup-bridge-contracts.nix
+++ nix/rollup-bridge-contracts.nix
@@ -33,6 +33,25 @@ stdenv.mkDerivation rec {
     sha256 = "1mhww44ni55yfcyn4hjql2hwnvag40p78kac7jjw2g2jdwwyb1fv";
   };
 
+  forgeStd = pkgs.fetchzip {
+    url = "https://github.com/foundry-rs/forge-std/archive/refs/tags/v1.9.6.zip";
+    sha256 = "sha256-4y1Hf0Te2oJxwKBOgVBEHZeKYt7hs+wTgdIO+rItj0E=";
+  };
+
+  solmate = pkgs.fetchFromGitHub {
+    owner = "transmissions11";
+    repo = "solmate";
+    rev = "c93f7716c9909175d45f6ef80a34a650e2d24e56";
+    sha256 = "sha256-zv8Jzap34N5lFVZV/zoT/fk73pSLP/eY427Go3QQM/Y="; # Replace with actual hash
+  };
+
+  dsTest = pkgs.fetchFromGitHub {
+    owner = "dapphub";
+    repo = "ds-test";
+    rev = "e282159d5170298eb2455a6c05280ab5a73a4ef0";
+    sha256 = "sha256-wXtNq4ZUohndNGs9VttOI9m9VW5QlVKOPtR8+mv2fBM=";
+  };
+
   buildPhase = ''
     echo "Installing soljson"
     (cd create-nil-hardhat-project; bash install_soljson.sh ${soljson26})
@@ -49,16 +68,17 @@ stdenv.mkDerivation rec {
     pwd
     cp .env.example .env
 
+    echo "Copying Foundry libraries from Nix store"
+    mkdir -p lib/forge-std lib/solmate lib/ds-test
+    cp -r ${forgeStd}/* lib/forge-std
+    cp -r ${solmate}/* lib/solmate
+    cp -r ${dsTest}/* lib/ds-test
+
     echo "Installing Node.js dependencies..."
     npm install  # Ensure node_modules exists before compiling
 
     echo "Start Hardhat compiling:"
     #npx hardhat clean && npx hardhat compile
-
-    echo "installing forge-std"
-    forge install foundry-rs/forge-std
-    forge install transmissions11/solmate
-    forge install dapphub/ds-test
 
     echo "Start Forge compiling:"
     forge compile --root $FOUNDRY_ROOT
