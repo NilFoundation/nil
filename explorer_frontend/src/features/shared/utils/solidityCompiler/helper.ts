5a5fc03de5d808e98fdb5318b87e9f1cefa7bc56
Jemiiah <seunemokhare@gmail.com>
2025-03-25 18:37:07 +0100

    rm comments

--- explorer_frontend/src/features/shared/utils/solidityCompiler/helper.ts
+++ explorer_frontend/src/features/shared/utils/solidityCompiler/helper.ts
@@ -1,8 +1,3 @@
-// TODO: get rid of hardcoded imports
-// import FaucetSol from "@nilfoundation/smart-contracts/contracts/Faucet.sol";
-// import NilSol from "@nilfoundation/smart-contracts/contracts/Nil.sol";
-// import NilTokBaseSol from "@nilfoundation/smart-contracts/contracts/NilTokenBase.sol";
-// import SmartAccountSol from "@nilfoundation/smart-contracts/contracts/SmartAccount.sol";
 import { processImports } from "./processImports";
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -10,31 +5,7 @@ export const createCompileInput = async (contractBody: string, options: any = {}
   const sources: Record<string, { content: string }> = {
     Compiled_Contracts: {
       content: contractBody,
-    },
-    // "Faucet.sol": {
-    //   content: FaucetSol,
-    // },
-    // "@nilfoundation/smart-contracts/contracts/Faucet.sol": {
-    //   content: FaucetSol,
-    // },
-    // "NilTokenBase.sol": {
-    //   content: NilTokBaseSol,
-    // },
-    // "@nilfoundation/smart-contracts/contracts/NilTokenBase.sol": {
-    //   content: NilTokBaseSol,
-    // },
-    // "Nil.sol": {
-    //   content: NilSol,
-    // },
-    // "@nilfoundation/smart-contracts/contracts/Nil.sol": {
-    //   content: NilSol,
-    // },
-    // "SmartAccount.sol": {
-    //   content: SmartAccountSol,
-    // },
-    // "@nilfoundation/smart-contracts/contracts/SmartAccount.sol": {
-    //   content: SmartAccountSol,
-    // },
+    }
   };
 
   await processImports(contractBody, "", sources);
