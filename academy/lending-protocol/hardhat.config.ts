323c89ee9d90b99e265a93c9de66ffb5f3ac208c
KevinMomanyi <kevinmomanyi900@gmail.com>
2025-03-29 13:55:24 +0300

    chore: switch to pnpm and update dependencies

--- academy/lending-protocol/hardhat.config.ts
+++ academy/lending-protocol/hardhat.config.ts
@@ -3,34 +3,25 @@ import "@nomicfoundation/hardhat-ignition-ethers";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-ignition-ethers";
 import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-toolbox";
+
 import * as dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 
 import "./task/run-lending-protocol";
 
-dotenv.config();
+dotenv.config(); // Load .env variables
 
 const config: HardhatUserConfig = {
-  ignition: {
-    requiredConfirmations: 1,
-  },
-  defaultNetwork: "nil",
-  solidity: {
-    version: "0.8.28", // or your desired version
-    settings: {
-      viaIR: true, // needed to compile router
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.28",
   networks: {
     nil: {
-      url: process.env.NIL_RPC_ENDPOINT,
+      url: process.env.NIL_RPC_ENDPOINT || "", // Ensure it's a string
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
 
 export default config;
+
+
