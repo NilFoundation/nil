8137ec4f66d4dad49b021c3c726dddb9e905f284
ukorvl <u.korotovskiy@nil.foundation>
2025-05-12 19:18:21 +0400

    feat: add new transaction history chart

--- explorer_backend/routes/shards.ts
+++ explorer_backend/routes/shards.ts
@@ -2,19 +2,32 @@ import { router, publicProcedure } from "../trpc";
 import z from "zod";
 import { ShardInfoSchema, getShardStats } from "../daos/shards";
 import { CacheType, getCacheWithSetter } from "../services/cache";
+import { fetchShardsGasPrice } from "../services/rpc";
 
 export const shardsRouter = router({
-  shardsStat: publicProcedure.output(z.array(ShardInfoSchema)).query(async () => {
-    const [stat] = await getCacheWithSetter(
-      "shardState",
-      () => {
-        return getShardStats();
-      },
-      {
-        type: CacheType.TIMER,
-        time: 60000,
-      },
-    );
-    return stat;
-  }),
+  shardsStat: publicProcedure
+    .output(z.array(ShardInfoSchema))
+    .query(async () => {
+      const [stat] = await getCacheWithSetter(
+        "shardState",
+        () => {
+          return getShardStats();
+        },
+        {
+          type: CacheType.TIMER,
+          time: 60000,
+        }
+      );
+      return stat;
+    }),
+  shardsGasPrice: publicProcedure
+    .output(z.record(z.bigint()))
+    .query(async () => {
+      try {
+        const gasPricemap = await fetchShardsGasPrice();
+        return gasPricemap;
+      } catch (e) {
+        throw new Error("Failed to fetch shards gas price", { cause: e });
+      }
+    }),
 });
--- explorer_frontend/src/features/shared/utils/formatNumber.ts
+++ explorer_frontend/src/features/shared/utils/formatNumber.ts
@@ -2,4 +2,13 @@ const formatNumber = (number: number) => {
   return new Intl.NumberFormat("en-US").format(number);
 };
 
-export { formatNumber };
+function formatToK(input: number): string {
+  if (Number.isNaN(input)) throw new Error("Input is not a number");
+
+  if (input >= 1_000_000) return `${(input / 1_000_000).toFixed(1).replace(/\.0$/, "")}M`;
+  if (input >= 1_000) return `${(input / 1_000).toFixed(1).replace(/\.0$/, "")}K`;
+
+  return formatNumber(input);
+}
+
+export { formatNumber, formatToK };
