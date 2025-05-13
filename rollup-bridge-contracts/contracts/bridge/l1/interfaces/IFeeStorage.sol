2d7a801f92e1ea47dddfce38e158ccf9cecb1205
Daniel Kogtev <oclaw@nil.foundation>
2025-05-13 12:53:24 +0100

    fix solc compilation

--- rollup-bridge-contracts/contracts/bridge/l1/interfaces/IFeeStorage.sol
+++ rollup-bridge-contracts/contracts/bridge/l1/interfaces/IFeeStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 /// @title IFeeStorage
 /// @notice Interface for the INilGasPriceOracle contract which also used by sync_committee.
