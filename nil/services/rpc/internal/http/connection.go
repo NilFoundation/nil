2e5b78202f73b2c29c36bda601a599e1ed9eb91c
Dmitrii Skopintsev <dmtrskv@nil.foundation>
2025-05-15 13:33:04 +0200

    Release 2025.05.15 (0.2.0)

--- nil/services/rpc/internal/http/connection.go
+++ nil/services/rpc/internal/http/connection.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	MaxRequestContentLength  = 1024 * 1024 * 32 // 32MB
-	minSupportedRevision     = 1185
-	minSupportedNiljsVersion = "0.24.0"
+	minSupportedRevision     = 1416
+	minSupportedNiljsVersion = "0.27.0"
 	niljsClientVersionPrefix = "niljs/"
 )
 
