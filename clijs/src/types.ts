39cc6b0b9b60089f51e870c56a90bb6535ed1dcd
rolaman <rolamanbuss@yandex.ru>
2025-05-27 15:31:31 +0400

    clijs: nilcli tests & build fix

--- clijs/src/types.ts
+++ clijs/src/types.ts
@@ -1,12 +1,20 @@
 import type { Hex } from "@nilfoundation/niljs";
 import { Args, Flags } from "@oclif/core";
 
+export const defaultHelp = async (ctx: any) => {
+  if (ctx.options.default) {
+    return `${ctx.options.default.toString()}`;
+  }
+}
+
 export const bigintFlag = Flags.custom<bigint>({
   parse: async (input) => BigInt(input),
+  defaultHelp,
 });
 
 export const bigintArg = Args.custom<bigint>({
   parse: async (input) => BigInt(input),
+  defaultHelp,
 });
 
 export const hexArg = Args.custom<Hex>({
@@ -18,4 +26,5 @@ export const tokenFlag = Flags.custom<{ id: Hex; amount: bigint }>({
     const [tokenId, amount] = input.split("=");
     return { id: tokenId as Hex, amount: BigInt(amount) };
   },
+  defaultHelp,
 });
