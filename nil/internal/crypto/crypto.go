20e3c838c1b7311b4261f52edcf67bc572508d87
Oleg Babin <babinoleg@mail.ru>
2025-05-29 14:16:05 +0400

    drop unused crypto functions
    Pubkey validation was used on early development stages. Right now it's used
    only in tests. So it's possible to drop it at all.

--- nil/internal/crypto/crypto.go
+++ nil/internal/crypto/crypto.go
@@ -5,13 +5,8 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 
-	gethcommon "github.com/ethereum/go-ethereum/common"
 	gethcrypto "github.com/ethereum/go-ethereum/crypto"
-	"github.com/holiman/uint256"
 )
-
-var secp256k1N = new(uint256.Int).SetBytes(gethcommon.FromHex(
-	"0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"))
 
 // PrivateKeyToEthereumFormat formats the private key in Ethereum format (hexadecimal)
 func PrivateKeyToEthereumFormat(priv *ecdsa.PrivateKey) string {
