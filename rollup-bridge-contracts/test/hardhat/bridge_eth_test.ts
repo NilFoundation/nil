09cf2db9e9913de917bd588e2c2e9811a58d38d7
defistar <kanthl@nil.foundation>
2025-06-11 18:20:32 +0530

    fix: generateL2RelayMessage for EthBriding test

--- rollup-bridge-contracts/test/hardhat/bridge_eth_test.ts
+++ rollup-bridge-contracts/test/hardhat/bridge_eth_test.ts
@@ -15,6 +15,8 @@ import type { Abi } from "abitype";
 import { getCheckSummedAddress } from "../../scripts/utils/validate-config";
 import { decodeFunctionResult, encodeFunctionData } from "viem";
 import { generateNilSmartAccount, loadNilSmartAccount, prepareNilSmartAccountsForUnitTest } from "../../task/nil-smart-account";
+import { generateL2RelayMessage } from "./generate-l2-relay-message";
+import { bigIntReplacer } from "../../deploy/config/config-helper";
 
 const l1EthBridgeAddress = '0x0001e0d8f4De4E838a66963f406Fa826cCaCA322';
 
@@ -509,6 +511,65 @@ describe("L2BridgeMessenger Contract", () => {
         }
 
         console.log(`successfully granted RELAYER role for ${ownerSmartAccount.address} on L2BridgeMessenger: ${l2BridgeMessengerProxyAddress}`);
+
+        const depositorAddressValue = "0xc8d5559BA22d11B0845215a781ff4bF3CCa0EF89";
+        const depositAmountValue = "1000000000000";
+        const l2DepositRecipientValue = depositRecipientSmartAccount.address;
+        const l2FeeRefundAddressValue = feeRefundSmartAccount.address;
+
+        try {
+            const messageSender = l1ETHBridgeProxyDummyAddress;
+            const messageTarget = l2ETHBridgeProxyAddress;
+            const messageType = "1";
+            const messageCreatedAt = Math.floor(Date.now() / 1000);
+            const messageExpiryTime = Math.floor(Date.now() / 1000) + 10000;
+            const ethDepositRelayMessage = generateL2RelayMessage(depositorAddressValue, depositAmountValue, l2DepositRecipientValue, l2FeeRefundAddressValue);
+            const nilGasLimit = "1000000";
+            const maxFeePerGas = "27500000";
+            const maxPriorityFeePerGas = "1250000";
+            const feeCredit = "27500000000000";
+            const messageNonce = 0;
+
+            const relayMessage = encodeFunctionData({
+                abi: L2BridgeMessengerJson.default.abi as Abi,
+                functionName: "relayMessage",
+                args: [messageSender, messageTarget, messageType, messageNonce, ethDepositRelayMessage, messageExpiryTime],
+            });
+
+            console.log(`generated message to relay on L2BridgeMessenger: ${relayMessage}`);
+
+            const relayEthDepositMessageResponse = await ownerSmartAccount.sendTransaction({
+                to: l2BridgeMessengerProxyAddress as `0x${string}`,
+                data: relayMessage,
+                feeCredit: BigInt(feeCredit),
+                maxFeePerGas: BigInt(maxFeePerGas),
+                maxPriorityFeePerGas: BigInt(maxPriorityFeePerGas)
+            });
+
+            console.log(`relayMessage transaction was done and awaiting for ProcessedReceipt`);
+
+            const relayEthDepositMessageTxnReceipts: ProcessedReceipt[] = await relayEthDepositMessageResponse.wait();
+
+            const relayedEthDepositMessageTxnReceipt: ProcessedReceipt = relayEthDepositMessageTxnReceipts[0] as ProcessedReceipt;
+
+            const outputReceipts: ProcessedReceipt[] = relayedEthDepositMessageTxnReceipt.outputReceipts as ProcessedReceipt[];
+
+            console.log(`outputReceipt is: ${JSON.stringify(outputReceipts, bigIntReplacer, 2)}`)
+
+            const outputReceipt: ProcessedReceipt = outputReceipts[0] as ProcessedReceipt;
+
+            console.log(`outputReceipt extracted as: ${JSON.stringify(outputReceipt, bigIntReplacer, 2)}`);
+
+            // check the first element in the ProcessedReceipt and verify if it is successful
+            if (!outputReceipt.success) {
+                console.error(`Failed to relay message
+            on the L2BridgeMessenger contract: ${l2BridgeMessengerProxyAddress}`);
+            } else {
+                console.log(`successfully relayed EthDepositMessage on to L2BridgeMessenger with transactionReceipt: ${JSON.stringify(relayEthDepositMessageTxnReceipts[0], bigIntReplacer, 2)}`);
+            }
+        } catch (err) {
+            console.error(`Failed to relay ethBridge message on to L2BridgeMessenger: ${JSON.stringify(err)}`);
+        }
     });
 });
 
