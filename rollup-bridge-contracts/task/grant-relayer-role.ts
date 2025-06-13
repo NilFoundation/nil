9f17eff06114f5e805d89ff56c6e2ee1496691ce
defistar <kanthl@nil.foundation>
2025-06-13 18:39:39 +0530

    feat: setuptestFixture for L2BridgeContracts and ETHBridge UnitTest

--- rollup-bridge-contracts/task/grant-relayer-role.ts
+++ rollup-bridge-contracts/task/grant-relayer-role.ts
@@ -23,11 +23,15 @@ task("grant-relayer-role", "Grant relayer role to the smart-account of relayer n
             throw Error(`Invalid L2BridgeMessengerJson ABI`);
         }
 
+        console.log(`Inside grant-relayer-role hardhat task`);
+
         const deployerAccount = await loadNilSmartAccount();
 
         if (!deployerAccount) {
             throw Error(`Invalid Deployer SmartAccount`);
         }
+
+        console.log(`loaded deloyerAccount`);
 
         const balance = await deployerAccount.getBalance();
 
@@ -39,8 +43,10 @@ task("grant-relayer-role", "Grant relayer role to the smart-account of relayer n
 
         validateAddress(l2NetworkConfig.l2BridgeMessengerConfig.l2BridgeMessengerContracts.l2BridgeMessengerProxy, "l2NetworkConfig.l2BridgeMessengerConfig.l2BridgeMessengerContracts.l2BridgeMessengerProxy");
 
-        const relayerAddress = await fetchRelayer();
+        const relayerAddress = deployerAccount.address;
         l2NetworkConfig.l2CommonConfig.relayer = relayerAddress;
+
+        console.log(`got relayer address as: ${relayerAddress}`);
 
         // Save the updated config
         saveNilNetworkConfig(networkName, l2NetworkConfig);
